c7189c5a2d7f010344469f73895e1398f6caae22
jasonparrott <jason.parrott@gmail.com>
2017-08-03 23:16:04 +0900

    Update BusinessLogic.java

--- mavenpipelineexample/src/main/java/org/company/BusinessLogic.java
+++ mavenpipelineexample/src/main/java/org/company/BusinessLogic.java
@@ -2,7 +2,7 @@ package org.company;
 
 public class BusinessLogic {
     public int doubleTheValue(int x) {
-        System.out.println("New Code Change2!");
+        System.out.println("New Code Change3!");
         return x * 2;
     }
 }
